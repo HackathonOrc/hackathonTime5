f6640e1042bc563682664655f7a899fdb6b8583e
João Felipe <jfnascimento99@gmail.com>
2021-07-06 00:23:09 -0300

    #9 Router config

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 
 // importa as rotas
 import homeRoutes from './homeRoutes';
-
+const userRoute = require ('../routes/userRoute')
 
 const router = Router();
 
 router.use('/', homeRoutes);
+router.use('/user', userRoute);
 
 export default router;
